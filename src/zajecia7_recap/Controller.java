17d55bf34a612af811ffcf3b45c8e0c010d139bf
Predator <p@p.com>
2017-09-16 12:53:10 +0200

    lista

--- src/zajecia7_recap/Controller.java
+++ src/zajecia7_recap/Controller.java
@@ -1,20 +1,38 @@
 package zajecia7_recap;
 
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static javafx.scene.input.KeyCode.G;
+import static javafx.scene.input.KeyCode.J;
 
 public class Controller {
     @FXML
@@ -25,6 +43,11 @@ public class Controller {
 
     @FXML
     Stage stage;
+
+    @FXML
+    VBox lewypanel;
+
+    
 
     String path = "res";
 
@@ -41,8 +64,48 @@ public class Controller {
         IMG_SIZE = Math.min(300,
                 (int)Math.min(mycanvas.getHeight() - 2 * IMG_PAD, mycanvas.getWidth() - 2 * IMG_PAD));
         System.out.println(IMG_SIZE);
+
+        ListView<String> list = new ListView<String>();
+        list.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        //FUNKCJA WYBIERANIA WYSWIETLANEGO OBRAZKA POPRZEZ KLIKANIE POZYCJI Z LISTY
+        list.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                int newIndex = files.indexOf(newValue);
+                if (newIndex < 0)
+                    return;
+                fileIndex = newIndex;
+                showPicture();
+
+            }
+        });
+        //==========================//
+
+        ObservableList<String> items = FXCollections.observableArrayList (
+                "Single", "Double", "Suite", "Family App");
+
+        List<String> ww = Arrays.asList("aa","bb","cc");
+        ObservableList<String> iii = FXCollections.observableList(files);
+
+        list.setItems(iii);
+//        list.set
+//lewypanel.onMouseClickedProperty();
+        lewypanel.getChildren().add(list);
+
+
+//        handleOnMouseClicked(lewypanel.getChildren().add(list));
+
+
     }
 
+
+
+
+    @FXML private void handleOnMouseClicked(MouseEvent event)
+    {
+//        System.out.println("You clicked rectangle: " + event.toString());
+    }
 
     public void sayIt() {
         System.out.println("sayit");
@@ -55,6 +118,11 @@ public class Controller {
     public void alertujUsera() {
         System.out.println("alert!");
     }
+
+
+
+
+
 
     public void showPicture() {
         if (files.size()==0) return;
@@ -99,13 +167,19 @@ public class Controller {
                 rescaledWidth, rescaledHeight);
 
     }
+public void setIndex(int index){
 
+    fileIndex = index;
+
+}
     public void nextFile() {
         fileIndex += 1;
         if (fileIndex>=files.size()) {
             fileIndex -= files.size();
         }
         showPicture();
+        System.out.println("Użytkownik kliknął przycisk naprzód");
+//        ("Użytkownik kliknął przycisk naprzód");
     }
 
     public void previousFile() {
@@ -114,6 +188,7 @@ public class Controller {
             fileIndex += files.size();
         }
         showPicture();
+        System.out.println("Użytkownik kliknął przycisk wstecz");
     }
 
     //przechodzi po folderze zczytanym z pola dirpath
@@ -132,6 +207,8 @@ public class Controller {
         }
         fileIndex = 0;
         System.out.println("Files: " + files);
+        System.out.println("Pliki: " + files);
+//        fileIndex = files.indexOf(selectedString);
     }
 
     /*
@@ -146,6 +223,10 @@ public class Controller {
 
         File selectedDirectory = chooser.showDialog(stage);
         System.out.println(selectedDirectory);
+
+    }
+
+    public void isSelected(){
 
     }
 
